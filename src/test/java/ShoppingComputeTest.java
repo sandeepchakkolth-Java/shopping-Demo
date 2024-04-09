954dbcdeb9fc9334295a2c4f302d890a6704d3b2
sandeepchakkolth-Java <166487709+sandeepchakkolth-Java@users.noreply.github.com>
2024-04-09 13:43:48 +0530

    update shopping Test

--- src/test/java/ShoppingComputeTest.java
+++ src/test/java/ShoppingComputeTest.java
@@ -7,33 +7,33 @@ import java.util.List;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-public class ShoppingCalculatorTest {
+public class ShoppingComputeTest {
 
     @Test
     public void canCalculateThePriceOfABasketOfApples() {
         List<String> basketOfApples = Arrays.asList("Apple", "Apple", "Apple");
-        ShoppingCalculator calc = new ShoppingCalculator(basketOfApples);
+        ShoppingCompute calc = new ShoppingCompute(basketOfApples);
         assertThat(BlockingObservable.from(calc.totalPrice()).last(), is(105));
     }
 
     @Test
     public void canCalculateThePriceOfABasketOfBananas() {
         List<String> basketOfBananas = Arrays.asList("Banana", "Banana", "Banana");
-        ShoppingCalculator calc = new ShoppingCalculator(basketOfBananas);
+        ShoppingCompute calc = new ShoppingCompute(basketOfBananas);
         assertThat(BlockingObservable.from(calc.totalPrice()).last(), is(60));
     }
 
     @Test
     public void canCalculateThePriceOfABasketOfMelons() {
         List<String> basketOfMelons = Arrays.asList("Melon", "Melon", "Melon");
-        ShoppingCalculator calc = new ShoppingCalculator(basketOfMelons);
+        ShoppingCompute calc = new ShoppingCompute(basketOfMelons);
         assertThat(BlockingObservable.from(calc.totalPrice()).last(), is(100));
     }
 
     @Test
     public void canCalculateThePriceOfABasketOfLimes() {
         List<String> basketOfMelons = Arrays.asList("Lime", "Lime", "Lime", "Lime");
-        ShoppingCalculator calc = new ShoppingCalculator(basketOfMelons);
+        ShoppingCompute calc = new ShoppingCompute(basketOfMelons);
         assertThat(BlockingObservable.from(calc.totalPrice()).last(), is(45));
     }
 
@@ -43,14 +43,14 @@ public class ShoppingCalculatorTest {
                                             "Apple", "Apple", "Apple",  // 105
                                             "Melon", "Melon", "Melon",  // 100
                                             "Lime", "Lime", "Lime", "Lime"); // 45
-        ShoppingCalculator calc = new ShoppingCalculator(basket);
+        ShoppingCompute calc = new ShoppingCompute(basket);
         assertThat(BlockingObservable.from(calc.totalPrice()).last(), is(310));
     }
 
     @Test
     public void canCalculateThePriceOfAnEmptyBasket() {
         List<String> basket = Arrays.asList();
-        ShoppingCalculator calc = new ShoppingCalculator(basket);
+        ShoppingCompute calc = new ShoppingCompute(basket);
         assertThat(BlockingObservable.from(calc.totalPrice()).last(), is(0));
     }
 }
