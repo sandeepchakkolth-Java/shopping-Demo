8f43bf42e1e2e0582ce9b745b1cddff79e692089
sandeepchakkolth-Java <166487709+sandeepchakkolth-Java@users.noreply.github.com>
2024-04-09 13:32:21 +0530

    New Changes

--- src/main/java/BasketItem.java
+++ src/main/java/BasketItem.java
@@ -1,15 +1,15 @@
 
-public class BasketItem {
+public class Items {
     private String name;
     private OfferEnum offer;
     private Integer unitPrice;
 
-    public static BasketItem Apple = new BasketItem("Apple", OfferEnum.NONE, 35);
-    public static BasketItem Banana = new BasketItem("Banana", OfferEnum.NONE, 20);
-    public static BasketItem Melon = new BasketItem("Melon", OfferEnum.BUY_ONE_GET_ONE_FREE, 50);
-    public static BasketItem Lime = new BasketItem("Lime", OfferEnum.BUY_THREE_FOR_THE_PRICE_OF_TWO, 15);
+    public static Items Apple = new Items("Apple", OfferEnum.NONE, 35);
+    public static Items Banana = new Items("Banana", OfferEnum.NONE, 20);
+    public static Items Melon = new Items("Melon", OfferEnum.BUY_ONE_GET_ONE_FREE, 50);
+    public static Items Lime = new Items("Lime", OfferEnum.BUY_THREE_FOR_THE_PRICE_OF_TWO, 15);
 
-    public BasketItem(String name, OfferEnum offer, Integer unitPrice) {
+    public Items(String name, OfferEnum offer, Integer unitPrice) {
          this.name = name; this.offer = offer; this.unitPrice = unitPrice;
     }
 
